14f01b1d5f9f093fdf87d6dff96d98d4d76ab679
Taman Neupane <taman.neupane@gmail.com>
2018-08-20 22:20:53 +0545

    small change

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 
 app.get('/test', function(req, res) {
-    res.send('Welcome to toilet server');
+    res.send('Welcome to toilet server for this time');
 })
 
 app.use('/', allRoutes);
